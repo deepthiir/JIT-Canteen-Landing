2aabae97ba5db3b0e715cf6aa97be9dc093e0388
thedevilx <aadithya794@gmail.com>
2021-01-10 19:58:16 +0530

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import { useLocation, Switch, Route } from 'react-router-dom';
+import React, { useRef, useEffect, useState } from 'react';
+import { useLocation, Switch, Route, Redirect } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
 import ReactGA from 'react-ga';
-
+import { auth } from './services/firebase'
 
 
 // Layouts
@@ -13,6 +13,8 @@ import LayoutDefault from './layouts/LayoutDefault';
 import Home from './views/Home';
 import SignInPage from './views/SignIn/SignInPage';
 import SignUpPage from './views/SignUp/SignUpPage';
+import MainHome from './views/MainHome/MainHome';
+import CartPage from './views/CartPage/CartPage';
 
 // Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
@@ -35,6 +37,27 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    auth.onAuthStateChanged((user) => {
+      if (user !== null) {
+        if (isMounted) {
+          setAuthenticated(true)
+        }
+      } else {
+        if (isMounted) {
+          setAuthenticated(false)
+        }
+      }
+    })
+
+    return () => {
+      isMounted = false;
+    }
+  }, [])
+
   return (
     <ScrollReveal
       ref={childRef}
@@ -43,9 +66,13 @@ function App() {
 
           <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
 
-          <Route exact path="/signin" render={() => <SignInPage></SignInPage>}></Route>
+          <Route exact path="/signin" render={() => authenticated ? <Redirect to='/mainhome'></Redirect> : <SignInPage></SignInPage>}></Route>
 
-          <Route exact path="/signup" render={() => <SignUpPage></SignUpPage>}></Route>
+          <Route exact path="/signup" render={() => authenticated ? <Redirect to='/mainhome'></Redirect> : <SignUpPage></SignUpPage>}></Route>
+
+          <Route exact path="/mainhome" render={() => authenticated ? <MainHome></MainHome> : <Redirect to = '/signin'></Redirect>}></Route>
+
+          <Route exact path="/cart" render={() => <CartPage></CartPage>}></Route>
 
         </Switch>
       )} />
